2a8d799b9acf1acf54dd9271853a1e95e024599d
keyurjivani <keyurjivani07rw@gmail.com>
2024-12-16 20:18:04 +0530

    food

--- Food/middleware/user.js
+++ Food/middleware/user.js
@@ -7,3 +7,20 @@ const checkuser = (req, res, next) => {
   
 module.exports = { checkuser };
   
+
+const User = require('../model/user.schema');
+
+const authenticateUser = async (req, res, next) => {
+  try {
+    const userId = req.cookies.userId;
+    if (!userId) return res.status(401).json({ error: "Authentication required" });
+    const user = await User.findById(userId);
+    if (!user) return res.status(401).json({ error: "Invalid user" });
+    req.user = user; 
+    next(); 
+  } catch (err) {
+    res.status(401).json({ error: "Authentication failed" });
+  }
+};
+
+module.exports = { authenticateUser };
